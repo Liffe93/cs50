190f4bfaeacda874470448630669ce6f06890ea5
Liffe <111470998+Liffe93@users.noreply.github.com>
2024-04-30 11:23:51 -0400

    Update phonebook.c to include counting the number of times someone is called

--- phonebook.c
+++ phonebook.c
@@ -9,5 +9,16 @@ int main (void)
   string phone = get_string("What's your phone number? "); 
 
   printf("Your age is %i. Your name is %s, and your phone number is %s.", age, name, phone);
-  
+
+  //if i have called less than 1 time? if so call them again 
+  int n;
+  do 
+    {
+      n = get_int("How many times have you called %s? ", phone);
+    }
+  while (n <= 2);
+    {
+      printf("You should call them more");
+    }
 }
+
